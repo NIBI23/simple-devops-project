2069006ec96cc89766da22b84123e31ec08aec3c
Ubuntu <ubuntu@ip-172-31-9-64.eu-north-1.compute.internal>
2026-06-03 07:04:57 +0000

    permission issue resolved

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello this is Nibin from DevOps Project!');
+    res.send('Hello this is Nibinraj from DevOps Project!');
 });
 
 app.listen(3000, () => {
